daa96531100eafdec21607b6e0a7e47ac897d7e9
AAYafar <alex.Yafarov@yandex.ru>
2023-01-17 20:14:12 +0300

    added first ScriptableObject

--- Assets/Scripts/ScriptableObjects/ItemScriptableOblect.cs
+++ Assets/Scripts/ScriptableObjects/ItemScriptableOblect.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemScriptableOblect : MonoBehaviour
+[CreateAssetMenu(fileName = "Item", menuName = "ScriptableObjects/Item")]
+public class ItemScriptableOblect : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string m_description;
 }
+
